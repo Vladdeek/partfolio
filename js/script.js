f5af14e30f37d5cdc12202db2eb9bc9409a92320
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2024-11-20 07:09:29 +0200

    ADD selection skills and projects

--- js/script.js
+++ js/script.js
@@ -1,17 +1,70 @@
-// анимации при скроле
-const title = document.querySelector('.title')
+const animPoint1 = document.getElementById('about')
+const aboutTitle = document.querySelector('.about')
+const image = document.querySelector('.img-container')
+const rezume = document.querySelector('.rezume-container')
 
-const observer = new IntersectionObserver((entries, observer) => {
+const anim1 = new IntersectionObserver((entries, observer) => {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
 			setTimeout(() => {
-				title.style.opacity = '1'
-				title.style.transform = 'scale(1)'
-			}, 250) // задержка 500 миллисекунд
-		} else {
-			title.style.opacity = '0'
-			title.style.transform = 'scale(0.25)'
+				aboutTitle.style.opacity = '1'
+				aboutTitle.style.transform = 'translateY(0px) scale(1)'
+				image.style.transform = 'translateX(0) translateY(0) scale(1)'
+				image.style.opacity = '1'
+				rezume.style.transform = 'translateX(0) translateY(0) scale(1)'
+				rezume.style.opacity = '1'
+			}, 250) // задержка 250 миллисекунд
+			observer.unobserve(entry.target) // Прекращаем отслеживание после анимации
 		}
 	})
 })
-observer.observe(title)
+
+anim1.observe(animPoint1)
+
+const animPoint2 = document.getElementById('skill')
+const skillTitle = document.querySelector('.skill')
+const skillCards = document.querySelectorAll('.skill-card')
+
+const anim2 = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			setTimeout(() => {
+				skillTitle.style.opacity = '1'
+				skillTitle.style.transform = 'translateY(0) scale(1)'
+				skillCards.forEach((card, index) => {
+					setTimeout(() => {
+						card.style.opacity = '1'
+						card.style.transform = 'scale(1)'
+					}, 50 * (index + 1)) // Увеличиваем задержку для каждой карточки
+				})
+			}, 250) // задержка 250 миллисекунд
+			observer.unobserve(entry.target) // Прекращаем отслеживание после анимации
+		}
+	})
+})
+
+anim2.observe(animPoint2)
+
+const animPoint3 = document.getElementById('project')
+const projectTitle = document.querySelector('.project')
+const projectCards = document.querySelectorAll('.project-card')
+
+const anim3 = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			setTimeout(() => {
+				projectTitle.style.opacity = '1'
+				projectTitle.style.transform = 'translateY(0) scale(1)'
+				projectCards.forEach((projectCard, index) => {
+					setTimeout(() => {
+						projectCard.style.opacity = '1'
+						projectCard.style.transform = 'scale(1)'
+					}, 50 * (index + 1)) // Увеличиваем задержку для каждой карточки
+				})
+			}, 250) // задержка 250 миллисекунд
+			observer.unobserve(entry.target) // Прекращаем отслеживание после анимации
+		}
+	})
+})
+
+anim3.observe(animPoint3)
